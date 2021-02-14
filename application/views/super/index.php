d24d94f88deae00a1d10509171b700182d17d048
MrFrost-Nv27 <nazruelyourgoodfriend@gmail.com>
2021-02-15 05:52:36 +0700

    Menambahkan indikator calon santri admin super

--- application/views/super/index.php
+++ application/views/super/index.php
@@ -1,3 +1,99 @@
 <!-- Content -->
-Taruh isi halamannya disini
+<h5 class="card-title text-center">Jumlah Calon Santri Baru</h5>
+<div class="row">
+    <div class="col-lg-6 col-xl-4">
+        <div class="card mb-3 widget-content bg-love-kiss">
+            <div class="widget-content-wrapper text-black">
+                <div class="widget-content-left">
+                    <div class="widget-heading">Clients</div>
+                    <div class="widget-subheading">Jumlah Calon Pendaftar</div>
+                </div>
+                <div class="widget-content-right">
+                    <div class="widget-numbers text-white"><span>0</span></div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-lg-6 col-xl-4">
+        <div class="card mb-3 widget-content bg-happy-green">
+            <div class="widget-content-wrapper text-black">
+                <div class="widget-content-left">
+                    <div class="widget-heading">Clients</div>
+                    <div class="widget-subheading">Jumlah Calon Pendaftar</div>
+                </div>
+                <div class="widget-content-right">
+                    <div class="widget-numbers text-white"><span>0</span></div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-lg-6 col-xl-4">
+        <div class="card mb-3 widget-content bg-arielle-smile">
+            <div class="widget-content-wrapper text-black">
+                <div class="widget-content-left">
+                    <div class="widget-heading">Clients</div>
+                    <div class="widget-subheading">Jumlah Calon Pendaftar</div>
+                </div>
+                <div class="widget-content-right">
+                    <div class="widget-numbers text-white"><span>0</span></div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-lg-6 col-xl-4">
+        <div class="card mb-3 widget-content bg-sunny-morning">
+            <div class="widget-content-wrapper text-black">
+                <div class="widget-content-left">
+                    <div class="widget-heading">Clients</div>
+                    <div class="widget-subheading">Jumlah Calon Pendaftar</div>
+                </div>
+                <div class="widget-content-right">
+                    <div class="widget-numbers text-white"><span>0</span></div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-lg-6 col-xl-4">
+        <div class="card mb-3 widget-content bg-mean-fruit">
+            <div class="widget-content-wrapper text-black">
+                <div class="widget-content-left">
+                    <div class="widget-heading">Clients</div>
+                    <div class="widget-subheading">Jumlah Calon Pendaftar</div>
+                </div>
+                <div class="widget-content-right">
+                    <div class="widget-numbers text-white"><span>0</span></div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-lg-6 col-xl-4">
+        <div class="card mb-3 widget-content bg-deep-blue">
+            <div class="widget-content-wrapper text-black">
+                <div class="widget-content-left">
+                    <div class="widget-heading">Clients</div>
+                    <div class="widget-subheading">Jumlah Calon Pendaftar</div>
+                </div>
+                <div class="widget-content-right">
+                    <div class="widget-numbers text-white"><span>0</span></div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="row">
+    <div class="col">
+        <div class="card mb-3 widget-content bg-asteroid">
+            <div class="widget-content-wrapper text-white">
+                <div class="widget-content-left">
+                    <div class="widget-heading">Total</div>
+                    <div class="widget-subheading">Jumlah Calon Pendaftar</div>
+                </div>
+                <div class="widget-content-right">
+                    <div class="widget-numbers text-white"><span>0</span></div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 <!-- End Content -->
